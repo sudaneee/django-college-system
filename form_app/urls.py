a45e984915e735a1228963bbcdb7c67fbdfee9c2
sudaneee <99607513+sudaneee@users.noreply.github.com>
2022-09-01 09:50:53 +0100

    my commit

--- form_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.form_home, name='form-home'),
-    path('application-form', views.application_form, name='application-form'),
-]
